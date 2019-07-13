593ef55e48d9f70410b5d4cb9b5f142219756e67
Naelton Santos <naeon02@gmail.com>
2019-07-12 23:23:58 -0300

    get e post

---
+++ 1-get.php
@@ -0,0 +1,14 @@
+
+<form action="processa_dados.php">
+    <p>Digite seu nome:
+        <input type="text" name="nome" size="30">
+    </p>
+    <p>Digite sua idade:
+        <input type="text" name="idade" size="3">
+    </p>
+    <p>
+        <input type="submit"  value="Enviar">
+    </p>
+</form>
+
+
