cc73b1b410670beb51f1cee987950ae16410b185
Naelton Santos <naeon02@gmail.com>
2019-07-14 20:36:26 -0300

    modificações

--- 9.3-login.php
+++ 9.3-login.php
@@ -10,21 +10,23 @@ $email = $conexao->real_escape_string($email);
 $senha = $conexao->real_escape_string($senha);
 
 // acesso ao banco de dados
-$resultado = $conexao->query("SELECT * FROM usuarios WHERE email='$email'");
+$resultado = $conexao->query("SELECT * FROM usuarios WHERE email = '$email'");
 $linhas = $resultado->num_rows;
+// var_dump($linhas); // 1
 if($linhas==0)  // testa se a consulta retornou algum registro
 {
 	echo "<html><body>";
 	echo "<p align=\"center\">E-mail não encontrado!</p>";
-	echo "<p align=\"center\"><a href=\"login.html\">Voltar</a></p>";
+	echo "<p align=\"center\"><a href=\"9.2-login.html\">Voltar</a></p>";
 	echo "</body></html>";
 }
 else
 {
-    $dados = $resultado->fetch_array();
-    echo "<pre>";
-    echo print_r($dados);
-	$senha_banco = $dados["senha"];
+    $dados = $resultado->fetch_array(MYSQLI_ASSOC);
+    var_dump($dados);
+    echo "<br>";
+    $senha_banco = $dados['senha'];
+    echo var_dump($senha_banco);
 	
    	if ($senha != $senha_banco) // confere senha
 	{
